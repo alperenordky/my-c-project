073b99861a85fb9b010508feab5750445d256bc7
Alperen Ordukaya <alperenordukaya956@gmail.com>
2026-02-08 15:41:48 +0300

    guncellenmis str_join fonksiyonu

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include "libft.h"
 char *ft_strjoin(char const *s1, char const *s2)
 {
     int total_len;
@@ -9,6 +11,10 @@ char *ft_strjoin(char const *s1, char const *s2)
         return(0);
     total_len = ft_strlen(s1) + ft_strlen(s2);
     i = 0;
+
+    ptr = (char *)malloc(sizeof(char)*(total_len + 1));
+    if(!ptr)
+        return(0);
     while(i < ft_strlen(s1))
     {
         ptr[i] = s1[i];
@@ -24,4 +30,20 @@ char *ft_strjoin(char const *s1, char const *s2)
     ptr[i] = '\0';
     return(ptr);
     
+}
+
+#include<stdio.h>
+int main()
+{
+    char *ptr;
+    char s1[] = "hello ";
+    char s2[] = "world";
+    ptr = ft_strjoin(s1,s2);
+    int size = ft_strlen(ptr);
+    int i = 0;
+    while(i < size)
+    {
+        printf("%c",ptr[i]);
+        i++;
+    }
 }
